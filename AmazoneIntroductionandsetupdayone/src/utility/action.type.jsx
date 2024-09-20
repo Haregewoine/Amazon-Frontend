e3837c3a0be921485d3041857debbcd6eb42ba42
Haregewoine <Haregewoineabire9@gmail.com>
2024-09-20 19:51:04 -0400

    update jsx

--- AmazoneIntroductionandsetupdayone/src/utility/action.type.jsx
+++ AmazoneIntroductionandsetupdayone/src/utility/action.type.jsx
@@ -1,3 +1,12 @@
 export const Type = {
-  ADD_TO_BASKET:"ADD_TO_BASKET",
+   ADD_TO_BASKET:"ADD_TO_BASKET",
+   REMOVE_FROM_BASKET:"REMOVE_FROM_BASKET",
 }
+
+
+
+
+
+// export const Type = {
+//   ADD_TO_BASKET:"ADD_TO_BASKET",
+// }
