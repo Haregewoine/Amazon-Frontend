521716cb9ea105ceb5f83f57922a72d4ab4214cf
Haregewoine <Haregewoineabire9@gmail.com>
2024-09-25 16:54:23 -0400

    update pay method

--- AmazoneIntroductionandsetupdayone/src/Pages/Payment/Payment.jsx
+++ AmazoneIntroductionandsetupdayone/src/Pages/Payment/Payment.jsx
@@ -1,14 +1,34 @@
 import React from "react";
 import classes from "./Payment.module.css";
 import Layout from "../../Component/Layout/Layout";
+
 import { DataContext } from "../../Component/DataProvider/DataProvider";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import ProductCard from "../../Component/Product/ProductCard";
+import { useStripe, useElements, CardElement } from "@stripe/react-stripe-js";
 
 function Payment() {
   const [{ basket, user }, dispatch] = useContext(DataContext);
   //console.log(user);
   const totalItem = basket?.reduce((amount, item) => item.amount + amount, 0);
+
+const total = basket.reduce((amount, item) => {
+  return item.price * item.amount + amount;
+}, 0);
+
+
+
+
+  const [cardError, setCardError] = useState(null);
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleChange = (e) => {
+    console.log(e);
+    e?.error?.message?.setCardError(e?.error?.message);
+    setCardError(null);
+  };
+
   return (
     <Layout>
       {/* header */}
@@ -39,10 +59,25 @@ function Payment() {
         {/* card form */}
         <div className={classes.flex}>
           <h3>Payment method</h3>
-          <div>
-            <div>
+          <div className={classes.payment__card__container}>
+            <div className={classes.payment__details}>
               <form action="">
+                {cardError && (
+                  <small style={{ color: "red" }}>{cardError}</small>
+                )}
+                {/* card element */}
+                <CardElement onChange={handleChange} />
 
+                {/* price */}
+                <div className={classes.payment__price}>
+                  <div>
+                    <span style={{ display: "flex", gap: "10px" }}>
+                      <p>Total Order |</p> <currencyFormat amount={total} />
+                    </span>
+                  </div>
+
+                  <button>pay Now</button>
+                </div>
               </form>
             </div>
           </div>
