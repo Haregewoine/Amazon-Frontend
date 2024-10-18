0f1222a172c2a41aefb00bf5bdfe54b47191df74
Haregewoine <Haregewoineabire9@gmail.com>
2024-10-17 20:08:41 -0400

    deploying on netlify

--- AmazoneIntroductionandsetupdayone/src/Api/axios.js
+++ AmazoneIntroductionandsetupdayone/src/Api/axios.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
+  //local instance of firebase function
   //baseURL: "http://127.0.0.1:5001/clone-fcd01/us-central1/api",
-  
-  baseURL:"https://api-nvrrtywamq-uc.a.run.app"
+  //deployed version of firebase function
+  //baseURL:"https://api-nvrrtywamq-uc.a.run.app"
+  //deployed version of amazon server on render.com
+  baseURL:"https://amazon-api-deploy-3-ccmr.onrender.com",
   //baseURL: "http://127.0.0.1:5000/clone-fcd01/us-central1/api",
 });
 
